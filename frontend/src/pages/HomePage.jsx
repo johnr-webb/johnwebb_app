32190576413d746b9113da69127f2223de1e9fa9
John Webb <johnwebb354@gmail.com>
2025-08-22 17:38:48 -0500

    Final refactor away from css var()

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -66,7 +66,7 @@ const HomePage = () => {
             >
               My Latest Projects
             </Typography>
-            <Typography sx={{ color: 'var(--text-color)' }}>
+            <Typography sx={{ color: 'text.primary' }}>
               Explore my portfolio of work, from web applications to open-source contributions.
             </Typography>
           </Paper>
