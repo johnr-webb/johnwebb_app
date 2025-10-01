2f928630298256e981db424700f9f690d3dcfb96
John Webb <johnwebb354@gmail.com>
2025-10-01 12:59:19 -0500

    Frontend: Updated to work with express locally. Hopefully will work in Dockerfile.
    Backend:
    Updated to run without SQL database connection
    
    Infra:
    Updated to correct cloud run configurations

--- frontend/server/app.js
+++ frontend/server/app.js
@@ -3,18 +3,20 @@ const express = require('express');
 
 const app = express();
 
+const FRONTEND_PATH = path.resolve(__dirname, '..', 'dist');
+
 app.use('/health', (_, res) => {
   res.status(200).send('OK');
 });
 
-app.use(express.static(path.resolve(__dirname, '../build')));
+app.use(express.static(FRONTEND_PATH));
 
 app.get(/.*/, (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+  res.sendFile(path.join(FRONTEND_PATH, 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Webb Solutions running on ${PORT}`);
+  console.log(`JohnWebbDotCom Frontend running on port ${PORT}`);
 });
